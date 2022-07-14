e134d773d46d495d41655335589d4df9d58f3215
MRiabov <maksymriabov2004@gmail.com>
2022-07-14 16:03:39 +0100

    If you are an HR, launch this!

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class Main {
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String path;
+        File file = new File("1.txt");
+        try {
+            file.createNewFile();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        try (InputStream fileInputStream = new FileInputStream(file)) {
+            int scanned;
+            do {
+                scanned = fileInputStream.read();
+                System.out.print((char)scanned);
+            } while (scanned != -1);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
 }
